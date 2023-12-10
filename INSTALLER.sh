7ef090ff6043d534b44f8330aec234fad1f5ae5f
anasfik <work@gwhyyy.com>
2023-12-10 18:14:28 -0500

    [ Add ] added conditionals to add the .flutter-spy export to the found shells found profile files

--- INSTALLER.sh
+++ INSTALLER.sh
@@ -14,13 +14,39 @@ then
     echo "Path already exists in .bashrc"
     exit 1
 else
-    echo "$new_path" >> ~/.bashrc
-    source ~/.bashrc
-    echo "Path added to .bashrc"
+    
+    onePresent=0
+    
+    if [ -f ~/.zshrc ]; then
+        echo "$new_path" >> ~/.zshrc
+        source ~/.zshrc
+        echo "Path added to .zshrc"
+        onePresent=1
+    fi
+    
+    if [ -f ~/.bashrc ]; then
+        echo "$new_path" >> ~/.bashrc
+        source ~/.bashrc
+        echo "Path added to .bashrc"
+        onePresent=1
+    fi
+    
+    if [ -f ~/.profile ]; then
+        echo "$new_path" >> ~/.profile
+        source ~/.profile
+        echo "Path added to .profile"
+        onePresent=1
+    fi
+    
+    if [ $onePresent -eq 0 ]; then
+        echo "No .bashrc, .zshrc or .profile found. Please add the following line to your shell config file:"
+        echo "$new_path"
+        exit 1
+    fi
 fi
 
-
 echo "Flutter Spy installed successfully!"
+echo "open a new terminal and run 'flutterspy' to get started."
 
 
 
