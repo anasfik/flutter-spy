648b79a7f71520fad618f31f344771403f87198a
Abhi <85984486+AbhiTheModder@users.noreply.github.com>
2024-03-05 14:32:21 +0530

    Update install_termux.sh
    Useless NDK Path setup removed, i.e, not required

--- install_termux.sh
+++ install_termux.sh
@@ -89,9 +89,7 @@ export ANDROID_HOME=$HOME/android-sdk
 export PATH=$PATH:$HOME/android-sdk/cmdline-tools/latest/bin
 export PATH=$PATH:$HOME/android-sdk/platform-tools
 export PATH=$PATH:$HOME/android-sdk/build-tools/34.0.4
-export PATH=$PATH:$HOME/android-sdk/ndk/$ndk_version
 export PATH=$PATH:$PREFIX/lib/jadx/bin
-export ANDROID_NDK_ROOT=$HOME/android-sdk/ndk/$ndk_version
 EOL
 elif [ -f "~/.zshrc" ]; then
   cat <<- EOL >> ~/.zshrc
@@ -99,9 +97,7 @@ export ANDROID_HOME=$HOME/android-sdk
 export PATH=$PATH:$HOME/android-sdk/cmdline-tools/latest/bin
 export PATH=$PATH:$HOME/android-sdk/platform-tools
 export PATH=$PATH:$HOME/android-sdk/build-tools/34.0.4
-export PATH=$PATH:$HOME/android-sdk/ndk/$ndk_version
 export PATH=$PATH:$PREFIX/lib/jadx/bin
-export ANDROID_NDK_ROOT=$HOME/android-sdk/ndk/$ndk_version
 EOL
 else
   cat <<- EOL >> $PREFIX/etc/bash.bashrc
@@ -109,8 +105,6 @@ export ANDROID_HOME=$HOME/android-sdk
 export PATH=$PATH:$HOME/android-sdk/cmdline-tools/latest/bin
 export PATH=$PATH:$HOME/android-sdk/platform-tools
 export PATH=$PATH:$HOME/android-sdk/build-tools/34.0.4
-export PATH=$PATH:$HOME/android-sdk/ndk/$ndk_version
 export PATH=$PATH:$PREFIX/lib/jadx/bin
-export ANDROID_NDK_ROOT=$HOME/android-sdk/ndk/$ndk_version
 EOL
 fi
